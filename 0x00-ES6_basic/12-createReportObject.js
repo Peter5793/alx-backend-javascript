cbc4039b0701aaaf0bc7a2c6b6081e412414ebd7
Peter5793 <masaba.peter68@gmail.com>
2021-07-18 20:34:24 +0300

    fix indentation

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
   return {
-	  allEmployees: {
-	    ...employeesList,
-	  },
-	  getNumberOfDepartments(employeesList) {
- 	    return Object.keys(employeesList).length;
-	  },
-  };
+    allEmployees: {
+    ...employeesList,
+  },
+    getNumberOfDepartments(employeesList) {
+    return Object.keys(employeesList).length;
+  },
+ };
 }
