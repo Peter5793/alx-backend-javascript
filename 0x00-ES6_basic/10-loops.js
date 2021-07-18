09a4edcbb72ea50be8fc88dab10b87b274cba995
Peter5793 <masaba.peter68@gmail.com>
2021-07-18 20:10:38 +0300

    fix variable

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const final = [];
-  for (const idx of array) {
-    final.push(appendString + Value);
+  for (const value of array) {
+    final.push(appendString + value);
   }
 
   return final;
