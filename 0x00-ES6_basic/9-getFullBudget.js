0123709eaab949fa3bd6c6a68c275abbe68ec5f8
Peter5793 <masaba.peter68@gmail.com>
2021-07-18 19:45:02 +0300

    fix indenttion

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -6,6 +6,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     ...budget,
     getIncomeInDollars: (income) =>`$${income}`,
     getIncomeInEuros: (income) =>`${income} euros`,
-   };
+  };
+    
   return fullBudget;
 }
