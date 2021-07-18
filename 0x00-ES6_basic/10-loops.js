5e0e40235803e39c767fadb1867ab8aee6a2b8a3
Peter5793 <masaba.peter68@gmail.com>
2021-07-18 20:02:46 +0300

    apply fo.. of

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const final = [];
+  for (const idx of array) {
+    final.push(appendString + Value);
+  }
+
+  return final;
+}
