77b3ee02aeddf2b8d88c032795cd6cfbbece28dc
Peter Lugalia <p.lugalia@launchpad.alx.app>
2021-07-22 13:45:26 +0300

    get list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  return [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Fransciso',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Fransciso',
+    },
+  ];
+}
