7748e4cf2c0f894d1e38a296d5b44007ba55939f
Peter Lugalia <p.lugalia@launchpad.alx.app>
2021-07-22 20:33:24 +0300

    filter by city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,12 @@
+// Filter that is based on the city
+
+const getStudentByLocation = (students, city) => {
+  const studentsLocation = students.filter(
+    (student) => student.location === city,
+  );
+
+  return studentsLocation;
+
+};
+
+export default getStudentByLocation;
